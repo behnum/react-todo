c79e261987f58e6d83aa75782fcec627cd482129
Behnam Baharmand <behnam@baharmand.com>
2016-08-14 15:32:10 +0430

    Add the first component, todos-list

--- src/comoponents/app.js
+++ src/comoponents/app.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import TodosList from './Todos-list';
+
+const todos = [
+{
+  task: 'Create an app',
+  isCompleted: false
+},
+{
+  task: 'Eat Cake',
+  isCompleted: true
+}
+];
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
         <h1>React Todo</h1>
+        <TodosList />
       </div>
     );
   }
